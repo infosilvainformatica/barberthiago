bc373677bae3fc7fada71172681d1a892c09ec9f
infosilvainformatica <infosilvainformatica@gmail.com>
2025-11-19 20:47:14 -0300

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // Importando biblioteca express
 import express from "express";
 import Agendamento from "./agendamentos.js";
+import path, { join } from "path";
 
 const app = express();
 
 // Middleware para ler JSON do corpo da requisição
 app.use(express.json());
+
+const _dirname = path.resolve();
+
+app.use(express.static(path.join(_dirname, "public")));
 
 // GET - Consulta os dados cadastrados
 app.get ("/agendamentos", async (req, res)=>{
